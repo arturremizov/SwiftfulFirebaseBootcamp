a3ddc3ecb98a6c59034ad930ab9b55c3e5f4b257
Artur Remizov <arturremizov@gmail.com>
2023-08-15 18:02:24 +0300

    Firebase Firestore: sorting, filtering, and querying

--- SwiftfulFirebaseBootcamp/Core/Products/ProductsView.swift
+++ SwiftfulFirebaseBootcamp/Core/Products/ProductsView.swift
@@ -10,8 +10,43 @@ import SwiftUI
 @MainActor
 final class ProductsViewModel: ObservableObject {
     
-    @Published private(set) var products: [Product] = []
+    enum SortOption: String, CaseIterable {
+        case none
+        case priceHigh
+        case priceLow
+        
+        var isPriceDescending: Bool? {
+            switch self {
+            case .priceHigh:
+                return true
+            case .priceLow:
+                return false
+            default:
+                return nil
+            }
+        }
+    }
     
+    enum CategoryOption: String, CaseIterable {
+        case none
+        case smartphones
+        case laptops
+        case fragrances
+        
+        var id: String? {
+            switch self {
+            case .none:
+                return nil
+            default:
+                return rawValue
+            }
+        }
+    }
+    
+    @Published private(set) var products: [Product] = []
+    @Published var selectedSortOption: SortOption = .none
+    @Published var selectedCategory: CategoryOption = .none
+
     private let productsManager: ProductsManager
     init(productsManager: ProductsManager) {
         self.productsManager = productsManager
@@ -19,6 +54,28 @@ final class ProductsViewModel: ObservableObject {
     
     func getAllProducts() async throws {
         self.products = try await productsManager.getAllProducts()
+    }
+    
+    func sortProducts(_ sortOption: SortOption) async throws {
+        self.selectedSortOption = sortOption
+        try await filterProducts(by: selectedCategory, sortBy: sortOption)
+    }
+    
+    func filterProducts(by categoryOption: CategoryOption) async throws {
+        self.selectedCategory = categoryOption
+        try await filterProducts(by: categoryOption, sortBy: selectedSortOption)
+    }
+    
+    private func filterProducts(by category: CategoryOption, sortBy sortOption: SortOption) async throws {
+        if let categoryId = category.id, let isPriceDescending = sortOption.isPriceDescending {
+            self.products = try await productsManager.getAllProductsSortedByPriceFor(category: categoryId, descending: isPriceDescending)
+        } else if let categoryId = category.id {
+            self.products = try await productsManager.getAllProductsFor(category: categoryId)
+        } else if let isPriceDescending = sortOption.isPriceDescending {
+            self.products = try await productsManager.getAllProductsSortedByPrice(descending: isPriceDescending)
+        } else {
+            self.products = try await productsManager.getAllProducts()
+        }
     }
     
 //    func downloadProductsAndUploadToFirebase() {
@@ -51,6 +108,31 @@ struct ProductsView: View {
         .navigationTitle("Products")
         .task {
             try? await viewModel.getAllProducts()
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Menu("Sort: \(viewModel.selectedSortOption.rawValue)") {
+                    ForEach(ProductsViewModel.SortOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.sortProducts(option)
+                            }
+                        }
+                    }
+                }
+             }
+
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Menu("Category: \(viewModel.selectedCategory.rawValue)") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.filterProducts(by: option)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- SwiftfulFirebaseBootcamp/Firestore/ProductsManager.swift
+++ SwiftfulFirebaseBootcamp/Firestore/ProductsManager.swift
@@ -30,12 +30,35 @@ final class ProductsManager: ObservableObject {
     }
 
     func getAllProducts() async throws -> [Product] {
-        let snapshot = try await productsCollection.getDocuments()
-        return try snapshot.documents.map { try $0.data(as: Product.self, decoder: decoder) }
+        return try await productsCollection.getDocuments(as: Product.self, decoder: decoder)
+    }
+    
+    func getAllProductsSortedByPrice(descending: Bool) async throws -> [Product] {
+        return try await productsCollection.order(by: "price", descending: descending)
+            .getDocuments(as: Product.self, decoder: decoder)
+    }
+    
+    func getAllProductsFor(category: String) async throws -> [Product] {
+        return try await productsCollection.whereField("category", isEqualTo: category)
+            .getDocuments(as: Product.self, decoder: decoder)
+    }
+    
+    func getAllProductsSortedByPriceFor(category: String, descending: Bool) async throws -> [Product] {
+        return try await productsCollection
+            .whereField("category", isEqualTo: category)
+            .order(by: "price", descending: descending)
+            .getDocuments(as: Product.self, decoder: decoder)
     }
     
     // MARK: - Helpers
     private func productDocument(productId: String) -> DocumentReference {
         return productsCollection.document(productId)
+    }
+}
+
+extension Query {
+    func getDocuments<T: Decodable>(as type: T.Type, decoder: Firestore.Decoder) async throws -> [T] {
+        let snapshot = try await getDocuments()
+        return try snapshot.documents.map { try $0.data(as: type, decoder: decoder) }
     }
 }
